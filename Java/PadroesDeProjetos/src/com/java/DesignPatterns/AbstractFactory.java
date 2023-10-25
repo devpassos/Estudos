1e2a19e7201771892a390acaa0064a6528d86ade
devpassos <fsp.passos@gmail.com>
2023-10-25 18:14:35 -0300

    Sincronizando com o WSL PG

---
+++ Java/PadroesDeProjetos/src/com/java/DesignPatterns/AbstractFactory.java
@@ -0,0 +1,20 @@
+package com.java.DesignPatterns;
+/**
+ * 
+ */
+
+/**
+ * @author Felipe.Passos
+ *
+ */
+public class AbstractFactory {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Java/PadroesDeProjetos/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Felipe.Passos
+ *
+ */
+module PadroesDeProjetos {
+}
