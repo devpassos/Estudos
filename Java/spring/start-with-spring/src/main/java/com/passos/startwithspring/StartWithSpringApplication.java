09127c80588706ddf5a6aa73b8820b20aae1b835
devpassos <fsp.passos@gmail.com>
2023-11-28 19:32:07 -0300

    Adicionando projeto spring

---
+++ Java/spring/start-with-spring/src/main/java/com/passos/startwithspring/StartWithSpringApplication.java
@@ -0,0 +1,13 @@
+package com.passos.startwithspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StartWithSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StartWithSpringApplication.class, args);
+	}
+
+}
---
+++ Java/spring/start-with-spring/src/test/java/com/passos/startwithspring/StartWithSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.passos.startwithspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartWithSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
